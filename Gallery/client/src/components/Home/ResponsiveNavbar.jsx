36791765c9ac74bfa5b0f11beb3c6ee3e0b29db9
YosephMTefera <153958406+YosephMTefera@users.noreply.github.com>
2023-12-28 15:43:34 +0300

    Update ResponsiveNavbar.jsx

--- Gallery/client/src/components/Home/ResponsiveNavbar.jsx
+++ Gallery/client/src/components/Home/ResponsiveNavbar.jsx
@@ -5,7 +5,12 @@ import { GrHomeRounded } from "react-icons/gr"
 const ResponsiveNavbar = () => {
   return (
     <div className="w-[50%] h-[100vh] p-2 fixed bottom-0 z-40 top-[80px] bg-white min-[700px]:hidden">
-        <div className="w-[95%] mt-[100px] mx-auto">
+        <div className="w-[95%] mt-[50px] mx-auto">
+            <div className="my-[50px]">
+          
+                <span className='text-[20px]  font-bold text-[#0D5C63] uppercase'>[MYGallery]</span>
+      
+          </div>
         <div className='flex flex-col gap-[50px] bg-white text-gray-500 font-bold'>
             <a href="/" className='h-[100%] flex items-center gap-[10px]'>
                 <GrHomeRounded className='text-[20px]' />
